8a3af6528cda72a34edb6382db59b1d629f1249e
Breno-MT <brenomtdev@gmail.com>
2025-08-05 16:54:25 -0300

    FEAT: created global variables

---
+++ src/pages/utils/globals.js
@@ -0,0 +1,3 @@
+export const backEndURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const appName = process.env.NEXT_PUBLIC_APP_NAME;
+export const appVersion = process.env.NEXT_PUBLIC_APP_VERSION;
