79c814b84e276ffbbb001cff874dce9382541854
Breno-MT <brenomtdev@gmail.com>
2025-08-05 18:30:04 -0300

    FEAT: added login and register user screen layout

---
+++ src/pages/user/login.js
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm";
+import Layout from "../layout";
+
+export default function LoginUser() {
+  return (
+    <Layout>
+      <main className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
+        <LoginForm />
+      </main>
+    </Layout>
+  );
+}
---
+++ src/pages/user/register.js
@@ -0,0 +1,12 @@
+import RegisterForm from "@/components/RegisterForm";
+import Layout from "../layout";
+
+export default function RegisterUser() {
+  return (
+    <Layout>
+      <main className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
+        <RegisterForm />
+      </main>
+    </Layout>
+  );
+}
