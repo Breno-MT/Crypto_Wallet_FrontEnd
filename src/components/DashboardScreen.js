bac7d266d436c5fd15f4d0b9644b35dcfd59d79f
Breno-MT <brenomtdev@gmail.com>
2025-08-10 02:27:37 -0300

    FEAT: created Dashboard (W.I.P)

---
+++ src/components/DashboardScreen.js
@@ -0,0 +1,9 @@
+import { appName, appVersion } from "@/pages/utils/globals";
+
+export default function DashboardScreen() {
+  return (
+    <div className="flex flex-col md:flex-row items-center bg-white rounded max-w-4xl w-full overflow-hidden">
+      <p className="text-black">Dashboard (W.I.P)</p>
+    </div>
+  );
+}
---
+++ src/pages/user/dashboard.js
@@ -0,0 +1,12 @@
+import DashboardScreen from "@/components/DashboardScreen";
+import Layout from "../layout";
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <main className="flex min-h-screen items-center justify-center bg-gray-50 px-4">
+        <DashboardScreen />
+      </main>
+    </Layout>
+  );
+}
