e5171f88c3924e043016cc2b35d1c8f588fb5181
Breno-MT <brenomtdev@gmail.com>
2025-08-10 03:00:17 -0300

    FEAT: added lang for registration, fixed prettier

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1,14 +1,25 @@
 export default {
-    login: {
-        title: "Sign In",
-        email: "Email",
-        email_tip: "youremail@example.com",
-        password: "Password",
-        submit: "Login",
-        dont_have_account_capitalized: "Don't have an account?",
-        click_here_capitalized: "Click here"
-    },
-    dashboard: {
-        title: "Dashboard (W.I.P)"
-    }
-}
+  login: {
+    title: "Sign In",
+    email: "Email",
+    email_tip: "youremail@example.com",
+    password: "Password",
+    submit: "Login",
+    dont_have_account_capitalized: "Don't have an account?",
+    click_here_capitalized: "Click here",
+  },
+  register: {
+    title: "Create an account",
+    username: "Username",
+    username_tip: "Enter your username",
+    email: "Email",
+    email_tip: "youremail@example.com",
+    password: "Password",
+    submit: "Create account",
+    already_have_account_capitalized: "Already have an account?",
+    click_here_capitalized: "Click here",
+  },
+  dashboard: {
+    title: "Dashboard (W.I.P)",
+  },
+};
--- src/i18n/locales/pt.js
+++ src/i18n/locales/pt.js
@@ -1,14 +1,25 @@
 export default {
-    login: {
-        title: "Entrar",
-        email: "Email",
-        email_tip: "seuemail@exemplo.com",
-        password: "Senha",
-        submit: "Logar",
-        dont_have_account_capitalized: "Não tem uma conta?",
-        click_here_capitalized: "Clique aqui"
-    },
-    dashboard: {
-        title: "Dashboard (Em progresso)"
-    }
-}
+  login: {
+    title: "Entrar",
+    email: "Email",
+    email_tip: "seuemail@exemplo.com",
+    password: "Senha",
+    submit: "Logar",
+    dont_have_account_capitalized: "Não tem uma conta?",
+    click_here_capitalized: "Clique aqui",
+  },
+  register: {
+    title: "Criar uma conta",
+    username: "Usuário",
+    username_tip: "Digite seu usuário",
+    email: "Email",
+    email_tip: "seuemail@exemplo.com",
+    password: "Senha",
+    submit: "Crie uma conta",
+    already_have_account_capitalized: "Já tem uma conta?",
+    click_here_capitalized: "Clique aqui",
+  },
+  dashboard: {
+    title: "Dashboard (Em progresso)",
+  },
+};
