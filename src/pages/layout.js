6c4922502ee4d776a79409c4faa726c0ad4c765d
Breno-MT <brenomtdev@gmail.com>
2025-08-10 03:06:07 -0300

    REFACT: moved Navbar to its folder

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,5 +1,5 @@
 import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
+import NavBar from "@/components/navbar/NavBar";
 
 export default function Layout({ children }) {
   return (
